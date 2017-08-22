f296c7c735641d321d92892ad65fadd14ccc1072
Soloman N <sxnorthrop15@gmail.com>
2017-08-22 08:11:23 -0400

    Renaming + Remove unused code

--- YMapExporter/QuaternionExtensions.cs
+++ YMapExporter/QuaternionExtensions.cs
@@ -6,7 +6,7 @@ namespace YMapExporter
     /// <summary>
     ///     Source: Codewalker (c) 2017
     /// </summary>
-    public static class QuaternionExtension
+    public static class QuaternionExtensions
     {
         public static Vector3 Multiply(this Quaternion a, Vector3 b)
         {
@@ -25,76 +25,6 @@ namespace YMapExporter
             return new Vector3(b.X * (1.0f - ayyy - azzz) + b.Y * (axyy - awzz) + b.Z * (axzz + awyy),
                 b.X * (axyy + awzz) + b.Y * (1.0f - axxx - azzz) + b.Z * (ayzz - awxx),
                 b.X * (axzz - awyy) + b.Y * (ayzz + awxx) + b.Z * (1.0f - axxx - ayyy));
-        }
-
-        public static Matrix ToMatrix(this Quaternion q)
-        {
-            var xx = q.X * q.X;
-            var yy = q.Y * q.Y;
-            var zz = q.Z * q.Z;
-            var xy = q.X * q.Y;
-            var zw = q.Z * q.W;
-            var zx = q.Z * q.X;
-            var yw = q.Y * q.W;
-            var yz = q.Y * q.Z;
-            var xw = q.X * q.W;
-            var result = new Matrix
-            {
-                M11 = 1.0f - 2.0f * (yy + zz),
-                M12 = 2.0f * (xy + zw),
-                M13 = 2.0f * (zx - yw),
-                M14 = 0.0f,
-                M21 = 2.0f * (xy - zw),
-                M22 = 1.0f - 2.0f * (zz + xx),
-                M23 = 2.0f * (yz + xw),
-                M24 = 0.0f,
-                M31 = 2.0f * (zx + yw),
-                M32 = 2.0f * (yz - xw),
-                M33 = 1.0f - 2.0f * (yy + xx),
-                M34 = 0.0f,
-                M41 = 0.0f,
-                M42 = 0.0f,
-                M43 = 0.0f,
-                M44 = 1.0f
-            };
-            return result;
-        }
-
-        public static Quaternion Euler(float x, float y, float z)
-        {
-            const float deg2Rad = (float) (Math.PI / 180.0);
-            return RotationYawPitchRoll(x * deg2Rad, y * deg2Rad, z * deg2Rad);
-        }
-
-        public static Quaternion Euler(Vector3 euler)
-        {
-            var eulerRad = euler * (float) (Math.PI / 180.0);
-            return RotationYawPitchRoll(eulerRad.X, eulerRad.Y, eulerRad.Z);
-        }
-
-        private static Quaternion RotationYawPitchRoll(float yaw, float pitch, float roll)
-        {
-            var result = new Quaternion();
-
-            var halfRoll = roll / 2;
-            var halfPitch = pitch / 2;
-            var halfYaw = yaw / 2;
-
-            var sinRoll = (float) Math.Sin(halfRoll);
-            var cosRoll = (float) Math.Cos(halfRoll);
-
-            var sinPitch = (float) Math.Sin(halfPitch);
-            var cosPitch = (float) Math.Cos(halfPitch);
-
-            var sinYaw = (float) Math.Sin(halfYaw);
-            var cosYaw = (float) Math.Cos(halfYaw);
-
-            result.X = cosYaw * sinPitch * cosRoll + sinYaw * cosPitch * sinRoll;
-            result.Y = sinYaw * cosPitch * cosRoll - cosYaw * sinPitch * sinRoll;
-            result.Z = cosYaw * cosPitch * sinRoll - sinYaw * sinPitch * cosRoll;
-            result.W = cosYaw * cosPitch * cosRoll + sinYaw * sinPitch * sinRoll;
-
-            return result;
         }
 
         public static Quaternion ToQuaternion(this Vector3 vect)
